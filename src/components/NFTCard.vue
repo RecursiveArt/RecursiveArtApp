c476b97303c82df1c7b3b327c40dc829dbd33000
Craig Laparo <gruppler+github@gmail.com>
2022-01-15 13:36:03 -0500

    Initial NFT card

---
+++ src/components/NFTCard.vue
@@ -0,0 +1,49 @@
+<template>
+  <q-card class="nft-card">
+    <q-item>
+      <q-item-section>
+        <q-item-label class="text-h6">{{ nft.title }}</q-item-label>
+        <q-item-label class="text-accent">{{ nft.owner }}</q-item-label>
+      </q-item-section>
+      <q-item-section v-if="$slots.header">
+        <slot name="header" />
+      </q-item-section>
+    </q-item>
+    <div class="nft-img" :style="{ backgroundImage: `url(${nft.imgURL})` }" />
+    <q-card-section>
+      <div class="text-subtitle">
+        <div class="text-bold">Price</div>
+        <div>{{ nft.price }} ETH</div>
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<style lang="scss">
+.nft-card {
+  flex-grow: 1;
+  min-width: 30%;
+  max-width: 350px;
+
+  .nft-img {
+    background-color: $grey-9;
+    background-size: contain;
+
+    &:after {
+      content: "";
+      display: block;
+      padding-bottom: 100%;
+    }
+  }
+}
+</style>
+
+<script>
+import { computed } from "vue";
+
+export default {
+  name: "NFTCard",
+
+  props: ["nft"]
+};
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,13 +1,70 @@
 <template>
-  <q-page class="flex flex-center">
-    Hello world
+  <q-page padding>
+    <div class="text-h5 q-mb-lg">
+      Marketplace
+    </div>
+
+    <div class="row q-gutter-lg">
+      <NFTCard v-for="nft in nfts" :key="nft.id" :nft="nft" />
+    </div>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
+import { useStore } from "vuex";
+
+import NFTCard from "../components/NFTCard";
 
 export default defineComponent({
-  name: "PageIndex"
+  name: "PageIndex",
+
+  components: { NFTCard },
+
+  setup() {
+    const store = useStore();
+
+    const nfts = [
+      {
+        id: "1111111111111111",
+        title: "Test 1",
+        owner: "0wn3r4ddr355",
+        imgURL: "",
+        price: 0.01
+      },
+      {
+        id: "2222222222222222",
+        title: "Test 2",
+        owner: "0wn3r4ddr355",
+        imgURL: "",
+        price: 0.02
+      },
+      {
+        id: "3333333333333333",
+        title: "Test 3",
+        owner: "0wn3r4ddr355",
+        imgURL: "",
+        price: 0.03
+      },
+      {
+        id: "4444444444444444",
+        title: "Test 4",
+        owner: "0wn3r4ddr355",
+        imgURL: "",
+        price: 0.04
+      },
+      {
+        id: "5555555555555555",
+        title: "Test 5",
+        owner: "0wn3r4ddr355",
+        imgURL: "",
+        price: 0.05
+      }
+    ];
+
+    return {
+      nfts
+    };
+  }
 });
 </script>
